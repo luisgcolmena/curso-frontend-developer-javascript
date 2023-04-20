a2f66dc9be3f9f33146227ae2cb19b50135f2df5
luisgcolmena <luisgcolmena@gmail.com>
2023-04-20 16:36:38 -0400

    Fusionando features de HTML y CSS. Agregando funcionalidades con JS.

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const navbarEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 const menuIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
+const shoppingCart = document.querySelector(".navbar-shopping-cart")
+const productDetail = document.querySelector(".product-detail")
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
 menuIcon.addEventListener('click', toggleMobileMenu)
+shoppingCart.addEventListener('click', toggleProductDetail)
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle("inactive")
@@ -12,4 +15,16 @@ function toggleDesktopMenu () {
 
 function toggleMobileMenu () {
     mobileMenu.classList.toggle("inactive")
+
+    if (!productDetail.classList.contains("inactive")) {
+        productDetail.classList.toggle("inactive")
+    }    
+}
+
+function toggleProductDetail () {
+    productDetail.classList.toggle("inactive")
+
+    if (!mobileMenu.classList.contains("inactive")) {
+        mobileMenu.classList.toggle("inactive")
+    }
 }
