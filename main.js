3afbdae31583539d6e7b7d1f1affb3e32d9dbb87
luisgcolmena <luisgcolmena@gmail.com>
2023-05-15 16:28:46 -0400

    Añadiendo los features de la Clase 12

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ const desktopMenu = document.querySelector(".desktop-menu")
 const menuIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 const shoppingCart = document.querySelector(".navbar-shopping-cart")
-const productDetail = document.querySelector(".product-detail")
+const shoppingCartDetail = document.querySelector(".shopping-cart-detail")
 const cardsContainer = document.querySelector(".cards-container")
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
 menuIcon.addEventListener('click', toggleMobileMenu)
-shoppingCart.addEventListener('click', toggleProductDetail)
+shoppingCart.addEventListener('click', toggleshoppingCartDetail)
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle("inactive")
 
-    if (!productDetail.classList.contains("inactive")) {
-        productDetail.classList.toggle("inactive")
+    if (!shoppingCartDetail.classList.contains("inactive")) {
+        shoppingCartDetail.classList.toggle("inactive")
     }
 }
 
@@ -22,13 +22,13 @@ function toggleMobileMenu () {
     mobileMenu.classList.toggle("inactive")
 
     //Si el menu del shopping cart esta activo, se desactiva con el onClick() al mobile menu.
-    if (!productDetail.classList.contains("inactive")) {
-        productDetail.classList.toggle("inactive")
+    if (!shoppingCartDetail.classList.contains("inactive")) {
+        shoppingCartDetail.classList.toggle("inactive")
     }    
 }
 
-function toggleProductDetail () {
-    productDetail.classList.toggle("inactive")
+function toggleshoppingCartDetail () {
+    shoppingCartDetail.classList.toggle("inactive")
 
     if (!mobileMenu.classList.contains("inactive")) {
         mobileMenu.classList.toggle("inactive")
